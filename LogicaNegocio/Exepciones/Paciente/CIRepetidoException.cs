fc7947a426f5203035167eaad036341c5c27eac3
Guille301 <guillermogau300@gmail.com>
2025-02-21 20:05:21 -0300

    Avanze en mensaje de validacion

---
+++ LogicaNegocio/Exepciones/Paciente/CIRepetidoException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.Exepciones.Paciente
+{
+    public class CIRepetidoException : Exception
+    {
+
+        public CIRepetidoException(string message) : base(message) { }
+
+
+    }
+}
